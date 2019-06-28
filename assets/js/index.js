e66e0ed407eda0f7166543315e497b6825cb7751
jessicapaolam <jessicapaolabm@gmail.com>
2019-06-28 00:23:31 -0300

    Adjustments for indentation

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,67 +3,67 @@ var elSelected = null;
 var dataItems = [];
 
 function load(url, callback) {
-    var xhr = new XMLHttpRequest();
-    var htmlItems = '';
+	var xhr = new XMLHttpRequest();
+	var htmlItems = '';
 
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status == 200) {
-            var data = JSON.parse(xhr.responseText);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status == 200) {
+			var data = JSON.parse(xhr.responseText);
 
-            data.forEach(function (item) {
-                dataItems.push(item);
+			data.forEach(function (item) {
+				dataItems.push(item);
 
-                htmlItems += createHtml(dataItems.length - 1);
-            });
+				htmlItems += createHtml(dataItems.length - 1);
+			});
 
-            document.getElementById('IdContainerAll').innerHTML += htmlItems;
-            selected(0);
-        }
-    }
+			document.getElementById('IdContainerAll').innerHTML += htmlItems;
+			selected(0);
+		}
+	}
 
-    xhr.open('GET', url, true);
-    xhr.send('');
+	xhr.open('GET', url, true);
+	xhr.send('');
 
 }
 
-load(url, function(){});
+load(url, function () { });
 
 function createHtml(index) {
-    var htmlItem = '';
+	var htmlItem = '';
 
-    if ((index + 1) % 3 == 1) {
-        htmlItem += '<div class="cards">';
-    }
+	if ((index + 1) % 3 == 1) {
+		htmlItem += '<div class="cards">';
+	}
 
-    htmlItem += '<div id="item_' + index.toString() + '" class="card-style item" onclick="selected(' + index.toString() + ', this)">';
-    htmlItem += '<div class="card-content">';
-	 htmlItem += '<img src="./assets/images/' + dataItems[index].foto + '">';
-	 htmlItem += '<div id="indicator">' + dataItems[index].indicador + '</div>';
-    htmlItem += '<div class="container">';
-    htmlItem += '<h4><b>' + dataItems[index].nome + '</b></h4>';
-    htmlItem += '<p>' + dataItems[index].cargo + '</p>';
-	 htmlItem += '</div>';
-    htmlItem += '</div>';
-    htmlItem += '</div>';
+	htmlItem += '<div id="item_' + index.toString() + '" class="card-style item" onclick="selected(' + index.toString() + ', this)">';
+	htmlItem += '<div class="card-content">';
+	htmlItem += '<img src="./assets/images/' + dataItems[index].foto + '">';
+	htmlItem += '<div id="indicator">' + dataItems[index].indicador + '</div>';
+	htmlItem += '<div class="container">';
+	htmlItem += '<h4><b>' + dataItems[index].nome + '</b></h4>';
+	htmlItem += '<p>' + dataItems[index].cargo + '</p>';
+	htmlItem += '</div>';
+	htmlItem += '</div>';
+	htmlItem += '</div>';
 
-    if ((index + 1) % 3 == 0) {
-        htmlItem += '</div>';
-    }
+	if ((index + 1) % 3 == 0) {
+		htmlItem += '</div>';
+	}
 
-    return htmlItem;
+	return htmlItem;
 }
 
 function selected(index) {
-    if(elSelected != null) {
-        elSelected.style.backgroundColor = '#f5f5f5';
-    }
+	if (elSelected != null) {
+		elSelected.style.backgroundColor = '#f5f5f5';
+	}
 
-    elSelected = document.getElementById('item_' + index.toString());
-    elSelected.style.backgroundColor = '#14314e';
+	elSelected = document.getElementById('item_' + index.toString());
+	elSelected.style.backgroundColor = '#14314e';
 
-    document.getElementById('photo').setAttribute('src', './assets/images/' + dataItems[index].foto);
-    document.getElementById('name').innerHTML = dataItems[index].nome;
-    document.getElementById('role').innerHTML = dataItems[index].cargo;
-	 document.getElementById('age').innerHTML = dataItems[index].idade;
-	 document.getElementById('indicator').innerHTML = dataItems[index].indicador;
+	document.getElementById('photo').setAttribute('src', './assets/images/' + dataItems[index].foto);
+	document.getElementById('name').innerHTML = dataItems[index].nome;
+	document.getElementById('role').innerHTML = dataItems[index].cargo;
+	document.getElementById('age').innerHTML = dataItems[index].idade;
+	document.getElementById('indicator').innerHTML = dataItems[index].indicador;
 }
