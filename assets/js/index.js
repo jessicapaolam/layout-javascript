295e8dc92dd4f487fa72349cfbed4a42bdb95ee7
jessicapaolam <jessicapaolabm@gmail.com>
2019-06-28 00:20:47 -0300

    COde refactoring and click implementation with status changes in the box

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,23 @@
-var url = 'dados.json';
-var dados = [];
+var url = 'assets/js/dados.json';
+var elSelected = null;
+var dataItems = [];
 
 function load(url, callback) {
     var xhr = new XMLHttpRequest();
+    var htmlItems = '';
 
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4 && xhr.status == 200) {
             var data = JSON.parse(xhr.responseText);
 
             data.forEach(function (item) {
-                dados.push(item);
+                dataItems.push(item);
+
+                htmlItems += createHtml(dataItems.length - 1);
             });
 
+            document.getElementById('IdContainerAll').innerHTML += htmlItems;
+            selected(0);
         }
     }
 
@@ -21,3 +27,43 @@ function load(url, callback) {
 }
 
 load(url, function(){});
+
+function createHtml(index) {
+    var htmlItem = '';
+
+    if ((index + 1) % 3 == 1) {
+        htmlItem += '<div class="cards">';
+    }
+
+    htmlItem += '<div id="item_' + index.toString() + '" class="card-style item" onclick="selected(' + index.toString() + ', this)">';
+    htmlItem += '<div class="card-content">';
+	 htmlItem += '<img src="./assets/images/' + dataItems[index].foto + '">';
+	 htmlItem += '<div id="indicator">' + dataItems[index].indicador + '</div>';
+    htmlItem += '<div class="container">';
+    htmlItem += '<h4><b>' + dataItems[index].nome + '</b></h4>';
+    htmlItem += '<p>' + dataItems[index].cargo + '</p>';
+	 htmlItem += '</div>';
+    htmlItem += '</div>';
+    htmlItem += '</div>';
+
+    if ((index + 1) % 3 == 0) {
+        htmlItem += '</div>';
+    }
+
+    return htmlItem;
+}
+
+function selected(index) {
+    if(elSelected != null) {
+        elSelected.style.backgroundColor = '#f5f5f5';
+    }
+
+    elSelected = document.getElementById('item_' + index.toString());
+    elSelected.style.backgroundColor = '#14314e';
+
+    document.getElementById('photo').setAttribute('src', './assets/images/' + dataItems[index].foto);
+    document.getElementById('name').innerHTML = dataItems[index].nome;
+    document.getElementById('role').innerHTML = dataItems[index].cargo;
+	 document.getElementById('age').innerHTML = dataItems[index].idade;
+	 document.getElementById('indicator').innerHTML = dataItems[index].indicador;
+}
